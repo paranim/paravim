b01d8376e2656b6257aba6204aade000633aa27c
oakes <zach@sekao.net>
2020-03-12 21:46:36 -0400

    Remove init code

--- src/paravim.nim
+++ src/paravim.nim
@@ -81,38 +81,3 @@ proc tick*(game: gl.RootGame) =
 
 proc isNormalMode*(): bool =
   libvim.vimGetMode() in {libvim.Normal.ord, libvim.NormalBusy.ord}
-
-when isMainModule:
-  doAssert glfwInit()
-
-  glfwWindowHint(GLFWContextVersionMajor, 4)
-  glfwWindowHint(GLFWContextVersionMinor, 1)
-  glfwWindowHint(GLFWOpenglForwardCompat, GLFW_TRUE) # Used for Mac
-  glfwWindowHint(GLFWOpenglProfile, GLFW_OPENGL_CORE_PROFILE)
-  glfwWindowHint(GLFWResizable, GLFW_TRUE)
-  glfwWindowHint(GLFWTransparentFramebuffer, GLFW_TRUE)
-
-  let w: GLFWWindow = glfwCreateWindowC(1024, 768, "Paravim", nil, nil)
-  if w == nil:
-    quit(-1)
-
-  w.makeContextCurrent()
-  glfwSwapInterval(1)
-
-  discard w.setKeyCallback(keyCallback)
-  discard w.setCharCallback(charCallback)
-  discard w.setMouseButtonCallback(mouseButtonCallback)
-  discard w.setCursorPosCallback(cursorPosCallback)
-  discard w.setFramebufferSizeCallback(frameSizeCallback)
-
-  var game = gl.RootGame()
-  let params = os.commandLineParams()
-  init(game, w, params)
-
-  while not w.windowShouldClose:
-    core.tick(game, true)
-    w.swapBuffers()
-    glfwPollEvents()
-
-  w.destroyWindow()
-  glfwTerminate()
