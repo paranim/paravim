53fc7c4e6a446db54f3732b492b6d3c51e44304f
oakes <zach@sekao.net>
2020-03-24 15:04:51 -0400

    Improve nim syntax highlighting - highlight public identifiers

--- src/paravim/colors.nim
+++ src/paravim/colors.nim
@@ -15,6 +15,9 @@ const
   tanColor* = glm.vec4(Glfloat(209/255), GLfloat(153/255), GLfloat(101/255), GLfloat(1.0))
   cyanColor* = glm.vec4(Glfloat(86/255), GLfloat(181/255), GLfloat(194/255), GLfloat(1.0))
   grayColor* = glm.vec4(Glfloat(150/255), GLfloat(129/255), GLfloat(133/255), GLfloat(1.0))
+  orangeColor* = glm.vec4(Glfloat(220/255), GLfloat(103/255), GLfloat(44/255), GLfloat(1.0))
+  redColor* = glm.vec4(Glfloat(210/255), GLfloat(45/255), GLfloat(58/255), GLfloat(1.0))
+  greenColor* = glm.vec4(Glfloat(65/255), GLfloat(174/255), GLfloat(122/255), GLfloat(1.0))
   syntaxColors* = {"string": tanColor,
                    "string_literal": tanColor,
                    "template_string": tanColor,
@@ -24,4 +27,5 @@ const
                    "float": yellowColor,
                    "comment": grayColor,
                    "op": grayColor,
+                   "public_id": cyanColor,
                    }.toTable
