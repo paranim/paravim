fa98ab4530eb895b7cf1cacad17be5bf4eec0dc1
oakes <zach@sekao.net>
2020-06-24 13:27:37 -0400

    Make conditional clearer

--- src/paravim/core.nim
+++ src/paravim/core.nim
@@ -488,7 +488,9 @@ proc updateTextEntity*(id: int, lines: RefStrings, parsed: tree_sitter.Nodes, te
     text.updateColors(e, parsed, lines, textColor)
   e.parsedNodes = parsed
   e.lineCount = lines[].len
-  if e.lineCount == 1 and lines[][0] == "":
+  # u_char_counts must not be empty
+  # because there is always at least one line
+  if e.uniforms.u_char_counts.data.len == 0:
     e.uniforms.u_char_counts.data = @[0.int32]
     e.uniforms.u_char_counts.disable = false
   e.uniforms.u_start_line.data = 0
