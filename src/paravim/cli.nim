5427395c821ec491cac7dd3c682675954257cbdb
oakes <zach@sekao.net>
2020-08-17 09:41:15 -0400

    Create terminal buffer every frame

--- src/paravim/cli.nim
+++ src/paravim/cli.nim
@@ -1,30 +1,27 @@
 from illwill as iw import nil
 import strutils
 
-var tb: iw.TerminalBuffer
-
 proc exitProc() {.noconv.} =
   iw.illwillDeinit()
   iw.showCursor()
   quit(0)
 
 proc init*() =
-
   iw.illwillInit(fullscreen=true)
   setControlCHook(exitProc)
   iw.hideCursor()
 
-  tb = iw.newTerminalBuffer(iw.terminalWidth(), iw.terminalHeight())
+proc tick*() =
+  var tb = iw.newTerminalBuffer(iw.terminalWidth(), iw.terminalHeight())
 
   iw.setForegroundColor(tb, iw.fgBlack, true)
-  iw.drawRect(tb, 0, 0, 40, 5)
+  iw.drawRect(tb, 0, 0, iw.width(tb)-1, iw.height(tb)-1)
   iw.drawHorizLine(tb, 2, 38, 3, doubleStyle=true)
 
   iw.write(tb, 2, 1, iw.fgWhite, "Press any key to display its name")
   iw.write(tb, 2, 2, "Press ", iw.fgYellow, "ESC", iw.fgWhite,
            " or ", iw.fgYellow, "Q", iw.fgWhite, " to quit")
 
-proc tick*() =
   var key = iw.getKey()
   case key
   of iw.Key.None: discard
@@ -32,4 +29,5 @@ proc tick*() =
   else:
     iw.write(tb, 8, 4, ' '.repeat(31))
     iw.write(tb, 2, 4, iw.resetStyle, "Key pressed: ", iw.fgGreen, $key)
+  iw.setCursorPos(tb, 0, 0)
   iw.display(tb)
