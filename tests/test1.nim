f59c4f0abba3142703cb9a7e7a92a1387a801012
oakes <zach@sekao.net>
2022-01-12 04:14:37 +0100

    Add unit test with ffi crash

--- tests/test1.nim
+++ tests/test1.nim
@@ -6,52 +6,10 @@
 # To run these tests, simply execute `nimble test`.
 
 import unittest
-from paranim/gl import nil
-from paravim/libvim import nil
-from paravim import nil
-from paravim/vim import nil
-import nimgl/glfw
+from paravim/tree_sitter import nil
 
-var game = gl.RootGame()
-
-proc init() =
-  doAssert glfwInit()
-
-  glfwWindowHint(GLFWContextVersionMajor, 3)
-  glfwWindowHint(GLFWContextVersionMinor, 3)
-  glfwWindowHint(GLFWOpenglForwardCompat, GLFW_TRUE) # Used for Mac
-  glfwWindowHint(GLFWOpenglProfile, GLFW_OPENGL_CORE_PROFILE)
-  glfwWindowHint(GLFWResizable, GLFW_TRUE)
-
-  let w: GLFWWindow = glfwCreateWindow(800, 600, "Paravim Test")
-  if w == nil:
-    quit(-1)
-
-  w.makeContextCurrent()
-
-  paravim.init(game, w)
-
-init()
-
-test "set the tab size":
-  libvim.vimOptionSetTabSize(2)
-  check libvim.vimOptionGetTabSize() == 2
-
-test "read a line":
-  let buf = libvim.vimBufferOpen("tests/hello.txt", 1, 0)
-  check libvim.vimBufferGetLine(buf, 1) == "Hello, world!"
-  libvim.vimExecute("bd!")
-
-test "delete all lines":
-  let buf = libvim.vimBufferOpen("tests/hello.txt", 1, 0)
-  check libvim.vimBufferGetLine(buf, 1) == "Hello, world!"
-  vim.onInput("g")
-  vim.onInput("g")
-  vim.onInput("d")
-  vim.onInput("G")
-  check libvim.vimBufferGetLine(buf, 1) == ""
-  vim.onInput("p")
-  check libvim.vimBufferGetLine(buf, 1) == "" # first line is blank
-  check libvim.vimBufferGetLine(buf, 2) == "Hello, world!"
-  vim.onInput("u")
-  libvim.vimExecute("bd!")
+test "parse nim file with tree sitter":
+  let
+    lines = @["echo \"Hello, world!\""]
+    (tree, parser) = tree_sitter.init("stuff.nim", lines)
+    parsed = tree_sitter.parse(tree, lines.len)
